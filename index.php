7f7209ef5d28c37e4c22a503c3ff89c3d55105cd
mahendripramadewi <50233839+mahendripramadewi@users.noreply.github.com>
2019-05-07 12:53:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="a.php" method="POST" enctype="multipart/form-data">
+<form action="process.php" method="POST" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="image" id="imaqe">
     <input type="submit" value="Upload Image" name="submit">
