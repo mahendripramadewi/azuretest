52c912b606295d2d0b303052cf9f4b7cb3fa44c7
mahendripramadewi <50233839+mahendripramadewi@users.noreply.github.com>
2019-05-07 21:58:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <form action="process.php" method="POST" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="image" id="imaqe">
     <input type="submit" value="Upload Image" name="submit">
 </form>
+
+</body>
+</html>
